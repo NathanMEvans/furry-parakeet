3a87eabf6ffc16e34457abd1a3f8b27351b9fdda
NathanMEvans <nathan@touchfantastic.com>
2018-03-12 19:09:23 +0000

    first cut at image and text

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="bg">
+          <img src={"https://scontent-lhr3-1.xx.fbcdn.net/v/t31.0-8/28701076_351988975297062_2331951337046330149_o.jpg?oh=343a67326afa7bc85f97b4cdf5a7517b&oe=5B00781D"} className="img" alt="logo" />
+          <div className="centered">Oxfordshire Liveable Streets</div>
+        </div>
       </div>
     );
   }
