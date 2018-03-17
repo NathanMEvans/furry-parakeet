3d85fc8c1967ab2553e9c3939dcc3866cd8ddc14
Nathan Evans <nathan@touchfantastic.com>
2018-03-17 17:49:21 +0000

    image top left

--- src/App.js
+++ src/App.js
@@ -6,11 +6,7 @@ class App extends Component {
     return (
       <div className="App">
         <div className="top">
-         <img src={logo} alt="logo" />
-        </div>
-        <div className="bg">
-          <img src={"https://scontent-lhr3-1.xx.fbcdn.net/v/t31.0-8/28701076_351988975297062_2331951337046330149_o.jpg?oh=343a67326afa7bc85f97b4cdf5a7517b&oe=5B00781D"} className="img" alt="logo" />
-          <div className="centered">Oxfordshire Liveable Streets</div>
+         <img src={logo} className='image' alt="logo" />
         </div>
       </div>
     );
