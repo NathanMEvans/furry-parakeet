cfc9043f1e07c1614ee651e529e95d32bdccb25f
Nathan Evans <nathan@touchfantastic.com>
2018-03-17 17:21:09 +0000

    add image

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import logo from './logo.png';
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <div className="top">
+         <img src={logo} alt="logo" />
+        </div>
         <div className="bg">
           <img src={"https://scontent-lhr3-1.xx.fbcdn.net/v/t31.0-8/28701076_351988975297062_2331951337046330149_o.jpg?oh=343a67326afa7bc85f97b4cdf5a7517b&oe=5B00781D"} className="img" alt="logo" />
           <div className="centered">Oxfordshire Liveable Streets</div>
